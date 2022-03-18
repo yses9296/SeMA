6750bb4406a6226ae1a80118bc7962ce02249a69
Eunseo Choi <yses9296@gmail.com>
2022-03-18 16:02:24 +0900

    Updated Cont01

--- code/assets/script/custom.js
+++ code/assets/script/custom.js
@@ -1,29 +1,64 @@
-//영화차트 이미지 슬라이드
-var swiper = new Swiper('.swiper-container',{
-    slidesPerView: 4,
-    spaceBetween: 24,
-//            mousewheel: {
-//                invert: true,
-//            },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-    },
-    autoplay: {
-        delay: 6000,
-    },
-    breakpoints: {
-        600: {
-            slidesPerView: 1.4,
-            spaceBetween: 24
+//Swiper
+$(document).ready(function (){    
+    var mySwiper = new Swiper('.swiper-container',{
+        slidesPerView: 5,
+        spaceBetween: 25,
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
         },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 24
+        loop: true,
+        autoplay: {
+            delay: 6000,
+            disableOnInteraction : false,
         },
-        960: {
-            slidesPerView: 3,
-            spaceBetween: 24
+        breakpoints: {
+            600: {
+                slidesPerView: 1.4,
+                spaceBetween: 24
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 24
+            },
+            960: {
+                slidesPerView: 3,
+                spaceBetween: 24
+            }
         }
-    }
+    });
 });
+
+//var height01 = $("#banner").height();
+//var height02 = $("#se").height();
+//var totalHeight = height01 + height02 + 50;
+
+var height = $("#container01").height();
+
+$("#aside").css("height", height);
+//$("#aside").css("padding-bottom", 2+"em");
+console.log(height);
+
+//var total = $("#aside").height();
+//console.log(height01);
+//console.log(height02);
+//console.log(total);
+
+//
+//var swiper = new Swier ('.swiper-container', {
+//    slidesPerView : 'auto',
+//    spaceBetween : 20,
+//});
+//
+//var slideSetting = {
+//    slidesPerView : 'auto',
+//    spaceBetween : 20,
+//    loop : false,
+//    loopAdditionalSlides : 1,
+//    autoplay : {
+//        delay : 3000,  
+//        disableOnInteraction : false, 
+//    }
+//};
+//
+//var slide = new Swiper('.swiper-container', slideSetting);
